89e78bbaf485fab46055fffee6ac066a4bdfea92
taiysuzu199@outlook.com <taiysuzu199@outlook.com>
2021-01-06 15:51:43 -0500

    almost completed game

--- air hockey/AirHockey.Designer.cs
+++ air hockey/AirHockey.Designer.cs
@@ -30,23 +30,22 @@ namespace air_hockey
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AirHockey));
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.netBox2 = new System.Windows.Forms.PictureBox();
             this.netBox1 = new System.Windows.Forms.PictureBox();
-            this.p1Striker = new System.Windows.Forms.PictureBox();
-            this.p2Striker = new System.Windows.Forms.PictureBox();
-            this.puck = new System.Windows.Forms.PictureBox();
             this.madeInPaint = new System.Windows.Forms.PictureBox();
             this.airHockeyTitle = new System.Windows.Forms.PictureBox();
-            this.p1ScoreLabel = new System.Windows.Forms.Label();
-            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.player1Image = new System.Windows.Forms.PictureBox();
+            this.puckImage = new System.Windows.Forms.PictureBox();
+            this.player2Image = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.netBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.netBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p1Striker)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p2Striker)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.puck)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.madeInPaint)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.airHockeyTitle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1Image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puckImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2Image)).BeginInit();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -75,43 +74,11 @@ namespace air_hockey
             this.netBox1.TabIndex = 2;
             this.netBox1.TabStop = false;
             // 
-            // p1Striker
-            // 
-            this.p1Striker.BackColor = System.Drawing.Color.Transparent;
-            this.p1Striker.BackgroundImage = global::air_hockey.Properties.Resources.blue;
-            this.p1Striker.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.p1Striker.Location = new System.Drawing.Point(190, 60);
-            this.p1Striker.Name = "p1Striker";
-            this.p1Striker.Size = new System.Drawing.Size(60, 60);
-            this.p1Striker.TabIndex = 3;
-            this.p1Striker.TabStop = false;
-            // 
-            // p2Striker
-            // 
-            this.p2Striker.BackColor = System.Drawing.Color.Transparent;
-            this.p2Striker.BackgroundImage = global::air_hockey.Properties.Resources.green;
-            this.p2Striker.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.p2Striker.Location = new System.Drawing.Point(190, 580);
-            this.p2Striker.Name = "p2Striker";
-            this.p2Striker.Size = new System.Drawing.Size(60, 60);
-            this.p2Striker.TabIndex = 4;
-            this.p2Striker.TabStop = false;
-            // 
-            // puck
-            // 
-            this.puck.BackColor = System.Drawing.Color.Transparent;
-            this.puck.BackgroundImage = global::air_hockey.Properties.Resources.puck;
-            this.puck.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.puck.Location = new System.Drawing.Point(190, 320);
-            this.puck.Name = "puck";
-            this.puck.Size = new System.Drawing.Size(60, 60);
-            this.puck.TabIndex = 5;
-            this.puck.TabStop = false;
-            // 
             // madeInPaint
             // 
             this.madeInPaint.BackColor = System.Drawing.Color.Black;
             this.madeInPaint.BackgroundImage = global::air_hockey.Properties.Resources.madeinpaint;
+            this.madeInPaint.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.madeInPaint.Location = new System.Drawing.Point(34, 650);
             this.madeInPaint.Name = "madeInPaint";
             this.madeInPaint.Size = new System.Drawing.Size(100, 50);
@@ -122,31 +89,45 @@ namespace air_hockey
             // 
             this.airHockeyTitle.BackColor = System.Drawing.Color.Black;
             this.airHockeyTitle.BackgroundImage = global::air_hockey.Properties.Resources.airhockey;
+            this.airHockeyTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.airHockeyTitle.Location = new System.Drawing.Point(34, 0);
             this.airHockeyTitle.Name = "airHockeyTitle";
             this.airHockeyTitle.Size = new System.Drawing.Size(100, 50);
             this.airHockeyTitle.TabIndex = 7;
             this.airHockeyTitle.TabStop = false;
             // 
-            // p1ScoreLabel
+            // player1Image
             // 
-            this.p1ScoreLabel.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.p1ScoreLabel.Location = new System.Drawing.Point(306, 0);
-            this.p1ScoreLabel.Name = "p1ScoreLabel";
-            this.p1ScoreLabel.Size = new System.Drawing.Size(100, 50);
-            this.p1ScoreLabel.TabIndex = 8;
-            this.p1ScoreLabel.Text = "0";
-            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.player1Image.BackColor = System.Drawing.Color.Transparent;
+            this.player1Image.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player1Image.BackgroundImage")));
+            this.player1Image.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.player1Image.Location = new System.Drawing.Point(185, 60);
+            this.player1Image.Name = "player1Image";
+            this.player1Image.Size = new System.Drawing.Size(70, 70);
+            this.player1Image.TabIndex = 8;
+            this.player1Image.TabStop = false;
             // 
-            // p2ScoreLabel
+            // puckImage
             // 
-            this.p2ScoreLabel.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.p2ScoreLabel.Location = new System.Drawing.Point(306, 650);
-            this.p2ScoreLabel.Name = "p2ScoreLabel";
-            this.p2ScoreLabel.Size = new System.Drawing.Size(100, 50);
-            this.p2ScoreLabel.TabIndex = 9;
-            this.p2ScoreLabel.Text = "0";
-            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.puckImage.BackColor = System.Drawing.Color.Transparent;
+            this.puckImage.BackgroundImage = global::air_hockey.Properties.Resources.puck;
+            this.puckImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.puckImage.Location = new System.Drawing.Point(195, 325);
+            this.puckImage.Name = "puckImage";
+            this.puckImage.Size = new System.Drawing.Size(50, 50);
+            this.puckImage.TabIndex = 9;
+            this.puckImage.TabStop = false;
+            // 
+            // player2Image
+            // 
+            this.player2Image.BackColor = System.Drawing.Color.Transparent;
+            this.player2Image.BackgroundImage = global::air_hockey.Properties.Resources.green;
+            this.player2Image.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.player2Image.Location = new System.Drawing.Point(185, 570);
+            this.player2Image.Name = "player2Image";
+            this.player2Image.Size = new System.Drawing.Size(70, 70);
+            this.player2Image.TabIndex = 10;
+            this.player2Image.TabStop = false;
             // 
             // AirHockey
             // 
@@ -156,13 +137,11 @@ namespace air_hockey
             this.BackgroundImage = global::air_hockey.Properties.Resources.air_hockey_2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(440, 700);
-            this.Controls.Add(this.p2ScoreLabel);
-            this.Controls.Add(this.p1ScoreLabel);
+            this.Controls.Add(this.player2Image);
+            this.Controls.Add(this.puckImage);
+            this.Controls.Add(this.player1Image);
             this.Controls.Add(this.airHockeyTitle);
             this.Controls.Add(this.madeInPaint);
-            this.Controls.Add(this.puck);
-            this.Controls.Add(this.p2Striker);
-            this.Controls.Add(this.p1Striker);
             this.Controls.Add(this.netBox1);
             this.Controls.Add(this.netBox2);
             this.DoubleBuffered = true;
@@ -175,11 +154,11 @@ namespace air_hockey
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.AirHockey_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.netBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.netBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p1Striker)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p2Striker)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.puck)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.madeInPaint)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.airHockeyTitle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1Image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puckImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2Image)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -189,13 +168,11 @@ namespace air_hockey
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.PictureBox netBox2;
         private System.Windows.Forms.PictureBox netBox1;
-        private System.Windows.Forms.PictureBox p1Striker;
-        private System.Windows.Forms.PictureBox p2Striker;
-        private System.Windows.Forms.PictureBox puck;
         private System.Windows.Forms.PictureBox madeInPaint;
         private System.Windows.Forms.PictureBox airHockeyTitle;
-        private System.Windows.Forms.Label p1ScoreLabel;
-        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.PictureBox player1Image;
+        private System.Windows.Forms.PictureBox puckImage;
+        private System.Windows.Forms.PictureBox player2Image;
     }
 }
 
--- air hockey/AirHockey.cs
+++ air hockey/AirHockey.cs
@@ -11,7 +11,24 @@ using System.Windows.Forms;
 namespace air_hockey
 {
     public partial class AirHockey : Form
-    {
+    {  
+        int striker1X = 185;
+        int striker1Y = 60;
+        int player1Score = 0;
+
+        int striker2X = 185;
+        int striker2Y = 570;
+        int player2Score = 0;
+
+        int strikerSize = 70;
+        int strikerSpeed = 8;
+
+        int puckX = 195;
+        int puckY = 325;
+        int puckSize = 50;
+        int puckXSpeed = 0;
+        int puckYSpeed = 0;
+
         bool wDown = false;
         bool sDown = false;
         bool aDown = false;
@@ -20,6 +37,12 @@ namespace air_hockey
         bool downArrowDown = false;
         bool leftArrowDown = false;
         bool rightArrowDown = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.DeepSkyBlue);
+        SolidBrush greenBrush = new SolidBrush(Color.LawnGreen);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Font screenFont = new Font("Consolas", 16);
 
         public AirHockey()
         {
@@ -90,12 +113,164 @@ namespace air_hockey
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
+            //move puck
+            puckX += puckXSpeed;
+            puckY += puckYSpeed;
 
+            //move player 1 
+            if (wDown == true && striker1Y > 50)
+            {
+                striker1Y -= strikerSpeed;
+            }
+
+            if (sDown == true && striker1Y < 260)
+            {
+                striker1Y += strikerSpeed;
+            }
+
+            if (aDown == true && striker1X > 20)
+            {
+                striker1X -= strikerSpeed;
+            }
+
+            if (dDown == true && striker1X < 350)
+            {
+                striker1X += strikerSpeed;
+            }
+
+            //move player 2 
+            if (upArrowDown == true && striker2Y > 370)
+            {
+                striker2Y -= strikerSpeed;
+            }
+
+            if (downArrowDown == true && striker2Y < 580)
+            {
+                striker2Y += strikerSpeed;
+            }
+
+            if (leftArrowDown == true && striker2X > 20)
+            {
+                striker2X -= strikerSpeed;
+            }
+
+            if (rightArrowDown == true && striker2X < 350)
+            {
+                striker2X += strikerSpeed;
+            }
+
+            //if puck hits left or right borders, change direction
+            if (puckX < 20 || puckX > 370)
+            {
+                puckXSpeed *= -1;
+                puckXSpeed--;
+            }
+            //top and bottom
+            if (puckY < 50 || puckY > 600)
+            {
+                puckYSpeed *= -1;
+                puckYSpeed--;
+            }
+
+            //create hitboxes for collison detetcion
+            Rectangle player1Rec = new Rectangle(striker1X, striker1Y, strikerSize, strikerSize);
+            Rectangle player2Rec = new Rectangle(striker2X, striker2Y, strikerSize, strikerSize);
+            Rectangle puckRec = new Rectangle(puckX, puckY, puckSize, puckSize);
+            Rectangle net1Rec = new Rectangle(165, 0, 110, 50);
+            Rectangle net2Rec = new Rectangle(165, 650, 110, 50);
+
+            //collision detection
+            if (player1Rec.IntersectsWith(puckRec))
+            {
+                if (puckXSpeed < 35 && puckYSpeed < 35)
+                {
+                    puckXSpeed += 15;
+                    puckYSpeed += 15;
+                    puckXSpeed *= -1;
+                }
+            }
+            if (player2Rec.IntersectsWith(puckRec))
+            {
+                if (puckXSpeed < 35 && puckYSpeed < 35)
+                {
+                    puckXSpeed -= 15;
+                    puckYSpeed -= 15;
+                    puckXSpeed *= -1;
+                }
+            }
+            //if puck intersects with net, add to score
+            if (puckRec.IntersectsWith(net1Rec))
+            {
+                player2Score++;
+                puckX = 195;
+                puckY = 325;
+                puckXSpeed = 0;
+                puckYSpeed = 0;
+                striker1X = 185;
+                striker1Y = 60;
+                striker2X = 185;
+                striker2Y = 570;
+            }
+            if (puckRec.IntersectsWith(net2Rec))
+            {
+                player1Score++;
+                puckX = 195;
+                puckY = 325;
+                puckXSpeed = 0;
+                puckYSpeed = 0;
+                striker1X = 185;
+                striker1Y = 60;
+                striker2X = 185;
+                striker2Y = 570;
+            }
+            //puck speed decreases over time
+            if (puckXSpeed > 0)
+            {
+                puckXSpeed--;
+            }
+            else if (puckXSpeed < 0)
+            {
+                puckXSpeed++;
+            }
+            else if (puckXSpeed == 0)
+            {
+                puckXSpeed = 0;
+            }
+
+            if (puckYSpeed > 0)
+            {
+                puckYSpeed--;
+            }
+            else if (puckYSpeed < 0)
+            {
+                puckYSpeed++;
+            }
+            else if (puckYSpeed == 0)
+            {
+                puckYSpeed = 0;
+            }
+
+            //check if either player has reached 3 points
+            if (player1Score == 3 || player2Score == 3)
+            {
+                gameTimer.Enabled = false;
+            }
+
+            Refresh();
         }
 
         private void AirHockey_Paint(object sender, PaintEventArgs e)
         {
+            Point puck = new Point(puckX, puckY);
+            puckImage.Location = puck;
 
+            Point striker1 = new Point(striker1X, striker1Y);
+            Point striker2 = new Point(striker2X, striker2Y);
+            player1Image.Location = striker1;
+            player2Image.Location = striker2;
+
+            e.Graphics.DrawString($"P1: {player1Score}", screenFont, whiteBrush, 310, 10);
+            e.Graphics.DrawString($"P2: {player2Score}", screenFont, whiteBrush, 310, 660);
         }
     }
 }
